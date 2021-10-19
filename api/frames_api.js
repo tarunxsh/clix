cf7cbcf924611a3ecf2080e3a2edc2f1c109d222
tarunesh1234 <taruneshkumar1234@gmail.com>
2021-10-19 16:47:45 +0530

    delete curl cmd

--- api/frames_api.js
+++ api/frames_api.js
@@ -14,3 +14,4 @@ module.exports = router;
 
 
 // curl -F "descp=wallpaper" -F "likes=10" -F "downloads=15" -F "img=@filepath" -X POST http://localhost:8000/api/frames
+// curl -X "DELETE" http://localhost:8000/api/frames/3
